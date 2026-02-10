0ffd0b014163fdc75391e62c2e26e7819b052a78
anjalisharma505152-design <anjalisharma505152@gmail.com>
2026-02-10 02:46:20 -0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,13 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=hello
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    a=hello
+NameError: name 'hello' is not defined. Did you mean: 'help'?
+>>> a=8 b=9
+SyntaxError: invalid syntax
+>>> a=2
+>>> b=3
+>>> print(a+b)
+5
